806706ae6ddf81bd4f2d90b3cafe0e8d24f7b887
Vaniko Nozadze <ivane.nozadze.1@btu.edu.ge>
2024-07-31 01:24:06 +0400

    Minor commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', loadComponent: () => import('./core/login/login.component').then(mod => mod.LoginComponent)},
+    {path: 'login', loadComponent: () => import('./core/login/login.component').then(mod => mod.LoginComponent)},
+    {path: 'register', loadComponent: () => import('./core/register/register.component').then(mod => mod.RegisterComponent)}
+];
